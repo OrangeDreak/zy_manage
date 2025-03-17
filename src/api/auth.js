31c03794942da526de795df10ead10698988303e
Lhg2323 <liaohongguang@liaohongguangdeMacBook-Pro.local>
2025-03-17 19:09:03 +0800

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,10 @@
 import request from '@/utils/request'
 
-const API_BASE_URL = 'localhost:8081/tp' // local
-
 export const auth = {
   // 用户登录
   login(data) {
     return request({
-      url: '${API_BASE_URL}/admin/user/login',
+      url: '/admin/user/login',
       method: 'post',
       data
     })
