c6cb714c87695e05aa949a2f7cf47eccd9aafc46
Lhg2323 <1558000617@qq.com>
2025-03-29 15:36:24 +0800

    commit

--- src/config/website.js
+++ src/config/website.js
@@ -2,13 +2,13 @@
  * 全局配置文件
  */
 export default {
-  title: "SifuBuy",
-  indexTitle: "SifuBuy Admin",
+  title: "Qcelf",
+  indexTitle: "Qcelf Admin",
   clientId: "saber", // 客户端id (后端暂时没改，如果改请注意)
   clientSecret: "saber_secret", // 客户端密钥 (后端暂时没改，如果改请注意)
   captchaMode: true, // 是否开启验证码模式
   logo: "S",
-  key: "SifuBuy", //配置主键,目前用于存储
+  key: "Qcelf", //配置主键,目前用于存储
   lockPage: "/lock",
   tokenTime: 100, // 秒
   //http的status默认放行不才用统一处理的,
